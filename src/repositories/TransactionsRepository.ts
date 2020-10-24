54f5cd3cd2451cbd4f4d3131393f233ba5da2562
Zackynson <crys.chb@hotmail.com>
2020-10-23 21:34:35 -0300

    desafios concluidos

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,17 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-return-assign */
 import Transaction from '../models/Transaction';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
 }
 
 class TransactionsRepository {
@@ -14,15 +22,36 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const incomeTransactions = this.transactions.filter(
+      transaction => transaction.type === `income`,
+    );
+
+    const outcomeTransactions = this.transactions.filter(
+      transaction => transaction.type === `outcome`,
+    );
+
+    const totalIncome = incomeTransactions.reduce((a, b) => a + b.value, 0);
+    const totalOutcome = outcomeTransactions.reduce((a, b) => a + b.value, 0);
+
+    const balance: Balance = {
+      income: totalIncome,
+      outcome: totalOutcome,
+      total: totalIncome - totalOutcome,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: TransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
